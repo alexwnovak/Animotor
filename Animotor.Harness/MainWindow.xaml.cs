a8b55fb9e587f5e598e9f3a6307bb93778c9b813
Alex Novak <alexwnovak@gmail.com>
2016-04-30 17:10:09 -0400

    +Empty WPF project to act as a test harness project

---
+++ Animotor.Harness/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Animotor.Harness
+{
+   public partial class MainWindow : Window
+   {
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
+   }
+}
